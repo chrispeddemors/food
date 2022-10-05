bd9afba8e53a3b97c813a959e99ed8732c162ba3
Chris <chrispeddemors@gmail.com>
2022-10-05 18:47:30 +0200

    Updated dashboard

--- admin/index.php
+++ admin/index.php
@@ -17,27 +17,61 @@
             ?> 
             <br>
             <div class="col-4 text-center">
-                <h1>5</h1>
+                <?php 
+                    //Create Query
+                    $sql = "SELECT * FROM tbl_category";
+                    //Execute
+                    $res = mysqli_query($conn, $sql);
+                    //Validate
+                    $count = mysqli_num_rows($res);
+
+                ?>
+                <h1><?php echo $count ?></h1>
                 <br>
                 Categories
             </div>
 
             <div class="col-4 text-center">
-                <h1>5</h1>
+                <?php 
+                    //Create Query
+                    $sql2 = "SELECT * FROM tbl_food";
+                    //Execute
+                    $res2 = mysqli_query($conn, $sql2);
+                    //Validate
+                    $count2 = mysqli_num_rows($res2);
+                ?>
+                <h1><?php echo $count2 ?></h1>
                 <br>
-                Categories
+                Foods
             </div>
 
             <div class="col-4 text-center">
-                <h1>5</h1>
+                <?php
+                //Create Query 
+                $sql3 = "SELECT * FROM tbl_order";
+                //Execute
+                $res3 = mysqli_query($conn, $sql3);
+                //Validate
+                $count3 = mysqli_num_rows($res3);
+                ?>
+                <h1><?php echo $count3 ?></h1>
                 <br>
-                Categories
+                Total orders
             </div>
 
             <div class="col-4 text-center">
-                <h1>5</h1>
+                <?php 
+                //Create query
+                $sql4 = "SELECT SUM(total) AS Total FROM tbl_order";
+                //Execute
+                $res4 = mysqli_query($conn, $sql4);
+                //Validate
+                $row = mysqli_fetch_assoc($res4);
+                $total_earned = $row['Total'];
+                ?>
+                <h1>$ <?php echo $total_earned ?></h1>
                 <br>
-                Categories
+                Total earned
             </div>
 
             <div class="clearfix"></div>
